f36b7f7ec02e867e62159ef0adb6ac3567a47841
萝莉 <6284136@qq.com>
2022-10-06 00:44:07 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,13 +33,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
+          href="https://bo.ke"
           rel="noopener noreferrer"
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            BO.KE
           </span>
         </a>
       </footer>
